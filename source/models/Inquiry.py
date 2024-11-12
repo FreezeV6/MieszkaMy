9026eb423a9f177f914f49f75722521dbd35cf91
FreezeV5 <tarabulakrzysztof@gmail.com>
2024-11-12 22:33:01 +0100

    Zmiana danych w tabelach, dodanie commitow do bazy po zapisie w formularza

--- source/models/Inquiry.py
+++ source/models/Inquiry.py
@@ -1,3 +1,5 @@
+from matplotlib.backend_tools import ToolQuit
+
 from source.database.db_init import db
 
 
@@ -5,7 +7,10 @@ class Inquiry(db.Model):
     __tablename__ = 'inquiries'
 
     inquiry_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
     message = db.Column(db.Text, nullable=True)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.property_id'), nullable=True)
+
