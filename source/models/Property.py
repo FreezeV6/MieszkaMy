8f5f37183d33b8dd7b7df2ef9a427a4c41391108
Krzysztof Tarabula <tarabulakrzysztof@gmail.com>
2025-01-23 00:01:12 +0100

    update added logo as button, assigned images to properties

--- source/models/Property.py
+++ source/models/Property.py
@@ -9,5 +9,6 @@ class Property(db.Model):
     prop_location = db.Column(db.String(50), nullable=False)
     price = db.Column(db.String(20), nullable=False)
     description = db.Column(db.Text, nullable=True)  # Optional field for more property details
+    image = db.Column(db.String(100), nullable=True)
     hit_counter = db.Column(db.Integer, default=0)  # New field to count hits
     property_area = db.Column(db.Numeric(10, 2), default=0.0)
